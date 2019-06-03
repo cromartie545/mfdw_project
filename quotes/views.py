193d781a00dff886d31ba8b5f18635aa6091b925
cromartie545 <hrc245@gmail.com>
2019-06-02 23:18:10 -0400

    The login bug removed

--- quotes/views.py
+++ quotes/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
-
-from django.http import HttpResponseRedirect
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -18,47 +16,50 @@ from pages.models import Page
 class Register(CreateView):
     template_name = 'registration/register.html'
     form_class = UserCreationForm
-    success_url  = reverse_lazy('register-success')
+    success_url = reverse_lazy('register-success')
 
     def form_valid(self, form):
         form.save()
         return HttpResponseRedirect(self.success_url)
 
-
-class QuoteView(LoginRequiredMixin,ListView):
-    Login_url=reverse_lazy('login')
-    #model = Quote
-    context_object_name = 'quote'
-
-    def get_content_data(self, **kwrags):
-        context = super(QuoteView, self).get_context_data(**kwrags)
-        content['page_list'] = Page.objects.all()
-        return context
-
-
 class QuoteList(LoginRequiredMixin, ListView):
     login_url = reverse_lazy('login')
-    #model = Quote
+    # model = Quote
     context_object_name = 'all_quotes'
+
     def get_queryset(self):
         return Quote.objects.filter(username=self.request.user)
-    def get_content_data(self, **kwrags):
-        context = super(QuoteList, self).get_context_data(**kwrags)
-        content['page_list'] = Page.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteList, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
         return context
+
+class QuoteView(LoginRequiredMixin, DetailView):
+    login_url = reverse_lazy('login')
+    # model = Quote
+    context_object_name = 'quote'
+
+    def get_queryset(self):
+        return Quote.objects.filter(username=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
 @login_required(login_url=reverse_lazy('login'))
 def quote_req(request):
     submitted = False
     if request.method == 'POST':
         form = QuoteForm(request.POST, request.FILES)
-
-
         if form.is_valid():
+            # form.save()
             quote = form.save(commit=False)
             try:
-               quote.username = request.user
+                quote.username = request.user
             except Exception:
-               pass
+                pass
             quote.save()
             return HttpResponseRedirect('/quote/?submitted=True')
     else:
